f533e65b94ab5219e56e79b6b20ffa832545d57d
Andreea Valentina Iote <47303830+Deea95@users.noreply.github.com>
2022-10-19 17:37:41 +0300

    Add files via upload

---
+++ JavaFXUI/src/com/clientserver/Main_Controller.java
@@ -0,0 +1,14 @@
+package com.clientserver;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Main_Controller implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    // Declararea variabilelor aplicatiei:
+
+    }
+}
